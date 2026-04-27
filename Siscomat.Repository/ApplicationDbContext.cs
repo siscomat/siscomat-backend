7c339c437dd257f6395dce3674c49b8419b235b3
jessica <jessymontano@hotmail.com>
2026-04-27 14:41:27 -0700

    update to net10.0

---
+++ Siscomat.Repository/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Siscomat.Core.Entities;
+
+namespace Siscomat.Repositories
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Gestor> Gestores { get; set; }
+        public DbSet<Participante> Participantes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Plantilla> Plantillas { get; set; }
+        public DbSet<Constancia> Constancias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Participante>()
+                .HasKey(p => p.Folio);
+            modelBuilder.Entity<Participante>()
+                .Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Gestor>()
+                .Property(g => g.Correo)
+                .IsRequired()
+                .HasMaxLength(150);
+            modelBuilder.Entity<Gestor>()
+                .HasIndex(g => g.Correo)
+                .IsUnique();
+            modelBuilder.Entity<Gestor>()
+                .Property(g => g.PasswordHash)
+                .IsRequired();
+
+            modelBuilder.Entity<Plantilla>()
+                .Property(p => p.path)
+                .IsRequired();
+        }
+    }
+}
